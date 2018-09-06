c80a70876bd1f9a79df4ce6138c2eb25fbcd9329
alanPriyatna <alanpriyatna@gmail.com>
2018-09-06 14:41:21 +0700

    Create blinking_led.ino

---
+++ Pertemuan 1/blinking_led.ino
@@ -0,0 +1,17 @@
+//led modul dihubungkan pada pin 9
+//pin 9 diberi nama "led"
+int led = 9;
+
+//fungsi setup() yang hanya dijalankan sekali
+void setup() {
+  //inisialisasi pin digital 9(led) sebagai output
+  pinMode(led, OUTPUT);
+}
+
+//fungsi loop() yang dijalankan terus-menerus selama arduino hidup
+void loop() {
+  digitalWrite(led, HIGH); //led on
+  delay(1000);
+  digitalWrite(led, LOW); //led off
+  delay(1000);
+}
