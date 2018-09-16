c467ed1836ab7b8229c426c18177fb95b059c32b
alanPriyatna <alanpriyatna@gmail.com>
2018-09-16 22:15:13 +0700

    Update buzzer_mario_bros.ino

--- Minggu 2/buzzer_mario_bros.ino
+++ Minggu 2/buzzer_mario_bros.ino
@@ -1,21 +1,3 @@
-/*
-  Arduino Mario Bros Tunes
-  With Piezo Buzzer and PWM
-
-  Connect the positive side of the Buzzer to pin 3,
-  then the negative side to a 1k ohm resistor. Connect
-  the other side of the 1 k ohm resistor to
-  ground(GND) pin on the Arduino.
-
-  by: Dipto Pratyaksa
-  last updated: 31/3/13
-  modified by DigiWare Team for Arduino 101 Starter Kit
-*/
-
-/*************************************************
-   Public Constants
- *************************************************/
-
 #define BUZZER_MODULE 3
 #define NOTE_B0  31
 #define NOTE_C1  33
