dfe4e03367051098ed0dd40ae9bb658242581aa3
alanPriyatna <alanpriyatna@gmail.com>
2018-09-23 07:09:21 +0700

    Create soilMoistureRelay.ino

---
+++ Minggu 5/soilMoistureRelay.ino
@@ -0,0 +1,19 @@
+#define sensorPin A0
+#define pumpPin 8
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(sensorPin, INPUT);
+    pinMode(pumpPin, OUTPUT);
+    Serial.begin(9600);
+    Serial.println("SMART IRIGRATION PUMP");
+    digitalWrite(pumpPin, HIGH);
+}
+void loop() {
+    // put your main code here, to run repeatedly:
+    int valSensor = analogRead(sensorPin);
+    if(valSensor >900){
+        digitalWrite(pumpPin, LOW); //turn on the pump
+    }else {
+        digitalWrite(pumpPin, HIGH); //turn off the pump
+    }
+}
