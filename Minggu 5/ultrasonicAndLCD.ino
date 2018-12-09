5e4033f5128bdba0750be937e55d8224d5004fec
alanPriyatna <alanpriyatna@gmail.com>
2018-12-09 12:58:51 +0700

    Create ultrasonicAndLCD.ino

---
+++ Minggu 5/ultrasonicAndLCD.ino
@@ -0,0 +1,39 @@
+#include <LiquidCrystal.h>
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int trigPin = 8;
+const int echoPin = 7;
+long duration;
+int distance;
+
+
+void setup() {
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+
+}
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Distance in cm");
+  
+  lcd.setCursor(1, 0);
+  lcd.print(distance);
+}
